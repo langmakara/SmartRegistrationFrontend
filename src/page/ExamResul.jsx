49fb490e69a2b6c38804bd534a1456de3c5f3f9d
langmakara <langmakara122@gmail.com>
2025-09-15 21:35:54 +0700

    Add routing and new pages for Home and Exam Results
    - Installed react-router-dom for routing
    - Created Home and ExamResul components
    - Updated App component to include routing

---
+++ src/page/ExamResul.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExamResul = () => {
+    return (
+        <div>
+            <h1>Exam Results</h1>
+            <p>No results to display.</p>
+        </div>
+    );
+};
+
+export default ExamResul;
